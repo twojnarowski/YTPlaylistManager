3fd200951850159722db3fc5911d1b423c52acea
twojnarowski <tym17@24x7.pl>
2024-03-01 18:45:25 +0100

    Cleanup after adding gitignore

--- YTPlaylistManager/YTPlaylistManager/Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace YTPlaylistManager.Data
-{
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
-}
