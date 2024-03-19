de161a771df109523e8deae26ddaba98f72fa731
twojnarowski <tym17@24x7.pl>
2024-03-19 21:14:21 +0100

    Feat: add v1 of playlist display

---
+++ YTPlaylistManager.Domain/Playlist.cs
@@ -0,0 +1,18 @@
+﻿namespace YTPlaylistManager.Domain;
+
+/// <summary>
+/// Stores information about a Youtube Playlist.
+/// </summary>
+public class Playlist
+{
+    public string Id { get; set; }
+    public string Link { get; set; }
+
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+    public string ImageUrl { get; set; }
+
+    public long ItemsCount { get; set; }
+}
